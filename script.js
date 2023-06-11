8d29e0e913468f5e7beda53bb47d7006b2faf75d
Bhushan <alberteinstein02.99792458>
2023-06-11 19:35:04 +0530

    Just right side transition remained

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ open.addEventListener("click", (e) => {
 close.addEventListener("click", (e) => {
   document.querySelector(".menu").classList.toggle("right-[300%]");
 });
+
+const animation1 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("myShow");
+    } else {
+      entry.target.classList.remove("myShow");
+    }
+  });
+});
+const animation2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("myShowRight");
+    } else {
+      entry.target.classList.remove("myShowRight");
+    }
+  });
+});
+const animation3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("popIn");
+    } else {
+      entry.target.classList.remove("popIn");
+    }
+  });
+});
+const demo1 = document.querySelectorAll(".myHidden");
+demo1.forEach((temp) => animation1.observe(temp));
+const demo2 = document.querySelectorAll(".myHiddenRight");
+demo2.forEach((temp) => animation2.observe(temp));
+const demo3 = document.querySelectorAll(".popOut");
+demo3.forEach((temp) => animation3.observe(temp));
